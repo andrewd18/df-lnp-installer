9039e4c3a234231b3565597ebc1d99a2fd623d61
Andrew Dorney <andrewd18@gmail.com>
2013-09-03 15:42:46 -0500

    Add v0.0.1. Pseudocode, ahoy!

---
+++ df-lnp-installer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+VERSION=0.0.1
+echo 'Dwarf Fortress LNP Linux Installer'
+echo 'Version:' $VERSION
+
+# Everyone loves pseudocode!
+#
+
+# If arg == version, output version.
+# 
+# Check for DF OS requirements.
+# Wget official DF.
+# Wget latest DFHack with soundsense.
+# Wget latest LNP GUI.
+# Wget each graphics pack.
+# Unzip DF.
+# Unzip DF_Hack on top of df_linux/
+# Unzip DF Hack plugins to df_linux/hack/plugins/
+# Unzip LNP.
+# Drop graphics packs into LNP/Graphics/
+# Drop custom lnp.yaml into LNP/.
+
